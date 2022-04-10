fa60eaa2d56b6a75a424f3609933da61bf5ef3e5
ipr0310 <frandysp2000@gmail.com>
2022-04-10 12:45:01 -0400

    Testing auth

--- features/home/components/Authenticator.tsx
+++ features/home/components/Authenticator.tsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+import {
+  createCredentialDefaultArgs,
+  getCredentialDefaultArgs,
+} from "./dummyCredentials";
+
 export const Authenticator = () => {
+  // register / create a new credential
+  const registerCredential = () => {
+    navigator.credentials
+      .create(createCredentialDefaultArgs)
+      .then((cred: any) => {
+        console.log("NEW CREDENTIAL", cred);
+
+        // normally the credential IDs available for an account would come from a server
+        // but we can just copy them from above...
+        const idList = [
+          {
+            id: cred.rawId,
+            transports: ["internal"],
+            type: "public-key",
+          },
+        ];
+
+        getCredentialDefaultArgs.publicKey.allowCredentials = idList;
+        return navigator.credentials.get(getCredentialDefaultArgs);
+      })
+      .then((assertion) => {
+        console.log("ASSERTION");
+        alert(assertion);
+      })
+      .catch((err) => {
+        console.log("ERROR");
+        alert(err);
+      });
+  };
+
   return (
     <div>
-      <button className="bg-amber-100 px-4 py-2 rounded-md w-full">
-        Authenticate
+      <p className="text-md mb-3 text-center">
+        Register a credential using the button below and choose if you would
+        like to authenticate using FaceID, your fingerprint or USB Security Key.
+      </p>
+
+      <button
+        onClick={registerCredential}
+        className="bg-amber-100 px-4 py-2 rounded-md w-full"
+      >
+        Register Credential
       </button>
     </div>
   );
---
+++ features/home/components/dummyCredentials.ts
@@ -0,0 +1,48 @@
+let challengeBytes = new Uint8Array(16).buffer;
+
+// sample arguments for registration
+export const createCredentialDefaultArgs: any = {
+  publicKey: {
+    // Relying Party (a.k.a. - Service):
+    rp: {
+      name: "Signum Network FOSS",
+    },
+
+    // User:
+    user: {
+      id: new Uint8Array(16),
+      name: "debbya@signum.network",
+      displayName: "Core Team",
+    },
+
+    pubKeyCredParams: [
+      {
+        type: "public-key",
+        alg: -7, // "ES256" as registered in the IANA COSE Algorithms registry
+      },
+      {
+        type: "public-key",
+        alg: -257, // Value registered by this specification for "RS256"
+      },
+    ],
+
+    attestation: "direct",
+    timeout: 60000,
+    excludeList: [],
+    challenge: challengeBytes,
+  },
+};
+
+// sample arguments for login
+export let getCredentialDefaultArgs: any = {
+  publicKey: {
+    timeout: 60000,
+    // allowCredentials: [newCredential] // see below
+    challenge: new Uint8Array([
+      // must be a cryptographically random number sent from a server
+      0x79, 0x50, 0x68, 0x71, 0xda, 0xee, 0xee, 0xb9, 0x94, 0xc3, 0xc2, 0x15,
+      0x67, 0x65, 0x26, 0x22, 0xe3, 0xf3, 0xab, 0x3b, 0x78, 0x2e, 0xd5, 0x6f,
+      0x81, 0x26, 0xe2, 0xa6, 0x01, 0x7d, 0x74, 0x50,
+    ]).buffer,
+  },
+};
